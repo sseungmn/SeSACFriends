ed4d523bbf82d8cf4af5522948215a76a32818d8
miniOS <seungmin.oh.dev@gmail.com>
2022-01-18 02:21:24 +0900

    build : init

---
+++ Pods/Target Support Files/Pods-SeSACFriendsTests/Pods-SeSACFriendsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SeSACFriendsTests : NSObject
+@end
+@implementation PodsDummy_Pods_SeSACFriendsTests
+@end
---
+++ SeSACFriends/Sources/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SeSACFriends
+//
+//  Created by SEUNGMIN OH on 2022/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
