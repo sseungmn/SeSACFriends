950f7aad3936c6b87c8bd37c94194510b9723582
miniOS <seungmin.oh.dev@gmail.com>
2022-01-19 15:38:55 +0900

    refactor : StateButton with Rx Extension
    StateButton을 조금 더 깔끔하게 만들기 위해서
    Rx Extension을 활용해서 리펙토링했다.

--- SeSACFriends/Sources/Components/Button+Component.swift
+++ SeSACFriends/Sources/Components/Button+Component.swift
@@ -23,62 +23,64 @@ class StateButton: UIButton {
         self.layer.cornerRadius = 5
         
         styleState
-            .observe(on: MainScheduler.instance)
-            .subscribe(
-                onNext: { state in
-                    print(state)
-                    self.changeStyle(state: state)
-                })
+            .bind(to: self.rx.styleState)
             .disposed(by: disposeBag)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    func changeStyle(state: ButtonStyleState) {
-        switch state {
-        case .inactive:
-            self.layer.borderColor = UIColor.gray4.cgColor
-            self.layer.borderWidth = 1
-            self.backgroundColor = .customWhite
-            self.setTitleColor(.customBlack, for: .normal)
-        case .fill:
-            self.layer.borderColor = nil
-            self.layer.borderWidth = 0
-            self.backgroundColor = .brandGreen
-            self.setTitleColor(.customWhite, for: .normal)
-        case .outline:
-            self.layer.borderColor = UIColor.brandGreen.cgColor
-            self.layer.borderWidth = 1
-            self.backgroundColor = .customWhite
-            self.setTitleColor(.brandGreen, for: .normal)
-        case .cancel:
-            self.layer.borderColor = nil
-            self.layer.borderWidth = 0
-            self.backgroundColor = .gray2
-            self.setTitleColor(.customBlack, for: .normal)
-        case .disable:
-            self.layer.borderColor = nil
-            self.layer.borderWidth = 0
-            self.backgroundColor = .gray6
-            self.setTitleColor(.gray3, for: .normal)
-        }
+}
+
+extension StateButton {
+    // style
+    func inactiveStyle() {
+        self.layer.borderColor = UIColor.gray4.cgColor
+        self.layer.borderWidth = 1
+        self.backgroundColor = .customWhite
+        self.setTitleColor(.customBlack, for: .normal)
+    }
+    func fillStyle() {
+        self.layer.borderColor = nil
+        self.layer.borderWidth = 0
+        self.backgroundColor = .brandGreen
+        self.setTitleColor(.customWhite, for: .normal)
+    }
+    func outlineStyle() {
+        self.layer.borderColor = UIColor.brandGreen.cgColor
+        self.layer.borderWidth = 1
+        self.backgroundColor = .customWhite
+        self.setTitleColor(.brandGreen, for: .normal)
+    }
+    func cancelStyle() {
+        self.layer.borderColor = nil
+        self.layer.borderWidth = 0
+        self.backgroundColor = .gray2
+        self.setTitleColor(.customBlack, for: .normal)
+    }
+    func disableStyle() {
+        self.layer.borderColor = nil
+        self.layer.borderWidth = 0
+        self.backgroundColor = .gray6
+        self.setTitleColor(.gray3, for: .normal)
     }
 }
 
-//extension Reactive where Base: StateButton {
-//    var styleState: ControlProperty<ButtonStyleState> {
-//        return base.rx.controlProperty(
-//            editingEvents:  [.allEvents],
-//            getter: { stateButton in
-//                stateButton.styleState
-//            },
-//            setter: { stateButton, value in
-//                if stateButton.styleState != value {
-//                    stateButton.styleState = value
-//                }
-//            }
-//        )
-//    }
-//}
+extension Reactive where Base: StateButton {
+    var styleState: Binder<ButtonStyleState> {
+        return Binder(self.base) { button, styleState in
+            switch styleState {
+            case .inactive:
+                button.inactiveStyle()
+            case .fill:
+                button.fillStyle()
+            case .outline:
+                button.outlineStyle()
+            case .cancel:
+                button.cancelStyle()
+            case .disable:
+                button.disableStyle()
+            }
+        }
+    }
+}
