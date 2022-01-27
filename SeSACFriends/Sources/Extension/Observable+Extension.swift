8380a37954632f9f781d8ea220c54d36f96fcfce
miniOS <seungmin.oh.dev@gmail.com>
2022-01-27 17:53:01 +0900

    feat : Observable Extensions

--- SeSACFriends/Sources/Extension/Observable+Extension.swift
+++ SeSACFriends/Sources/Extension/Observable+Extension.swift
@@ -21,12 +21,89 @@ extension SharedSequenceConvertibleType {
     }
 }
 
+// From RxSwiftExt
+extension ObservableType where Element: EventConvertible {
+
+    /**
+     Returns an observable sequence containing only next elements from its input
+     - seealso: [materialize operator on reactivex.io](http://reactivex.io/documentation/operators/materialize-dematerialize.html)
+     */
+    public func elements() -> Observable<Element.Element> {
+        return compactMap { $0.event.element }
+    }
+
+    /**
+     Returns an observable sequence containing only error elements from its input
+     - seealso: [materialize operator on reactivex.io](http://reactivex.io/documentation/operators/materialize-dematerialize.html)
+     */
+    public func errors() -> Observable<Swift.Error> {
+        return compactMap { $0.event.error }
+    }
+}
+
+
 infix operator <-> : DefaultPrecedence
 
 /**
- * Bind to each other
- * Reference : https://github.com/khoren93/SwiftHub
+ Two way binding operator between control property and relay
+ 
+ ### Reference
+ [RxSwift/RxExample](https://github.com/ReactiveX/RxSwift/blob/main/RxExample/RxExample/Operators.swift)
  */
+func nonMarkedText(_ textInput: UITextInput) -> String? {
+    let start = textInput.beginningOfDocument
+    let end = textInput.endOfDocument
+
+    guard let rangeAll = textInput.textRange(from: start, to: end),
+        let text = textInput.text(in: rangeAll) else {
+            return nil
+    }
+
+    guard let markedTextRange = textInput.markedTextRange else {
+        return text
+    }
+
+    guard let startRange = textInput.textRange(from: start, to: markedTextRange.start),
+        let endRange = textInput.textRange(from: markedTextRange.end, to: end) else {
+            return text
+    }
+
+    return (textInput.text(in: startRange) ?? "") + (textInput.text(in: endRange) ?? "")
+}
+
+@discardableResult
+func <-> <Base>(textInput: TextInput<Base>, variable: BehaviorRelay<String>) -> Disposable {
+    let bindToUIDisposable = variable.asObservable()
+        .bind(to: textInput.text)
+    let bindToVariable = textInput.text
+        .subscribe(onNext: { [weak base = textInput.base] value in
+            guard let base = base else {
+                return
+            }
+
+            let nonMarkedTextValue = nonMarkedText(base)
+
+            /**
+             In some cases `textInput.textRangeFromPosition(start, toPosition: end)` will return nil even though the underlying
+             value is not nil. This appears to be an Apple bug. If it's not, and we are doing something wrong, please let us know.
+             The can be reproed easily if replace bottom code with
+
+             if nonMarkedTextValue != variable.value {
+             variable.value = nonMarkedTextValue ?? ""
+             }
+
+             and you hit "Done" button on keyboard.
+             */
+            if let nonMarkedTextValue = nonMarkedTextValue, nonMarkedTextValue != variable.value {
+                variable.accept(nonMarkedTextValue)
+            }
+            }, onCompleted: {
+                bindToUIDisposable.dispose()
+        })
+
+    return Disposables.create(bindToUIDisposable, bindToVariable)
+}
+
 @discardableResult
 func <-> <T>(property: ControlProperty<T>, variable: BehaviorRelay<T>) -> Disposable {
     if T.self == String.self {
