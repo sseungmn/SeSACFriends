127e876ec48eb512d29241a48aca8cc3092518c4
miniOS <seungmin.oh.dev@gmail.com>
2022-02-02 02:27:23 +0900

    feat : userCardComponent
    여러곳에서 공통으로 사용될 UserCard를 Component로 만들었다.
    또한, 나중에 구매를 통해서 설정가능한 SesacCharacter, SesacBackground가
    변경됐을 때, 설정이 적용되도록 하기 위해서 Setting클래스를 만들어주었다.

---
+++ SeSACFriends/Sources/Components/UserCardImageView.swift
@@ -0,0 +1,36 @@
+//
+//  UserCardImageView.swift
+//  SeSACFriends
+//
+//  Created by SEUNGMIN OH on 2022/02/02.
+//
+
+import UIKit
+import SnapKit
+
+class UserCardView: View {
+    
+    let backgroundImageView = UIImageView().then { imageView in
+        imageView.image = Settings.shared.background.image
+    }
+    
+    let characterImageView = UIImageView().then { imageView in
+        imageView.image = Settings.shared.character.image
+    }
+    
+    override func configure() {
+        layer.masksToBounds = true
+        layer.cornerRadius = 8
+    }
+    
+    override func setConstraint() {
+        addSubview(backgroundImageView)
+        backgroundImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        addSubview(characterImageView)
+        characterImageView.snp.makeConstraints { make in
+            make.bottom.centerX.equalToSuperview()
+        }
+    }
+}
---
+++ SeSACFriends/Sources/Settings.swift
@@ -0,0 +1,55 @@
+//
+//  Settings.swift
+//  SeSACFriends
+//
+//  Created by SEUNGMIN OH on 2022/02/02.
+//
+
+import Foundation
+import UIKit
+
+class Settings {
+    static let shared = Settings()
+    
+    var character: SesacCharacter = .basic
+    var background: SesacBackground = .skyPark
+}
+
+// MARK: SesacCharacter
+enum SesacCharacter {
+    case basic, strong, mint, purple, gold
+}
+
+extension SesacCharacter {
+    var image: UIImage {
+        switch self {
+        case .basic:
+            return Asset.Assets.sesacFace1.image
+        case .strong:
+            return Asset.Assets.sesacFace2.image
+        case .mint:
+            return Asset.Assets.sesacFace3.image
+        case .purple:
+            return Asset.Assets.sesacFace4.image
+        case .gold:
+            return Asset.Assets.sesacFace5.image
+        }
+    }
+}
+
+// MARK: SesacBackground
+enum SesacBackground {
+    case skyPark, latinLivingRoom
+}
+
+extension SesacBackground {
+    var image: UIImage {
+        switch self {
+        case .skyPark:
+            return Asset.Assets.sesacBackground1.image
+        case .latinLivingRoom:
+            return Asset.Assets.sesacBackground2.image
+        }
+    }
+}
+
